31ba964e2a6e70b26949c462cdefe637772af82c
salvador.palmiciano <salvador.palmiciano@wirsolut.com>
2015-06-17 15:19:05 -0300

    add post method

--- services/controllers/perfiles.php
+++ services/controllers/perfiles.php
@@ -1,7 +1,24 @@
 <?php
 
-$app->get('/', function() use ($app) 
+$app->get('/perfiles', function() use ($app) 
 {
    	$data = $app['db']->getAll('SELECT * FROM perfiles');
  	return  $app->json($data);
+});
+
+$app->post('/perfiles', function() use ($app) 
+{
+	$name = $_POST['name'];
+
+	$sql = <<<SQL
+INSERT INTO perfiles ('name') VALUES ($name)
+SQL;
+	try {
+   		$response['status'] = 'success'; 
+   		$response['message'] = $app['db']->getAll($sql);
+	} catch (Exception $e) {
+   		$response['status'] = 'success'; 
+   		$response['message'] = $app['db']->getAll($sql);
+	}
+ 	return  $app->json($response);
 });
