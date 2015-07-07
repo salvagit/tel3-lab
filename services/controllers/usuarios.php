d81a78e2882751e162f92096197cc8eb2752c465
maximo <marucci.maximo@gmail.com>
2015-07-07 18:29:48 -0300

    add class 5

--- services/controllers/usuarios.php
+++ services/controllers/usuarios.php
@@ -7,14 +7,33 @@ use Symfony\Component\HttpFoundation\Response;
  * Get
  * 
  */
+$app->get('/hola', function() use ($app) 
+{
+      return 'hola';
+});
+
+/**
+ * Get
+ * 
+ */
 $app->get('/getusuarios', function() use ($app) 
 {
-   	$data = $app['db']->getAll('SELECT * FROM usuarios');
-   	
-   	foreach ($data as $k => &$v) {
-   		$v['actions'] = $app['twig']->render('usuarios/actions.twig', $v);
-   	}
+      $data = $app['db']->getAll('SELECT * FROM usuarios');
+      
+      foreach ($data as $k => &$v) {
+         $v['actions'] = $app['twig']->render('usuarios/actions.twig', $v);
+      }
 
+      return $app->json($data);
+});
+
+/**
+ * Get
+ * 
+ */
+$app->get('/getusuario/{id}', function($id) use ($app) 
+{
+   	$data = $app['db']->getAll("SELECT * FROM usuarios WHERE id = $id");
    	return $app->json($data);
 });
 
@@ -28,7 +47,7 @@ $app->get('/usuarios', function() use ($app)
 
    	// seteando parametros para la vista
    	$params['data'] = $data;
-   	$params['basepath'] = 'http://tel3.labs/';
+   	$params['basepath'] = 'http://tel3-labs.local/';
    	// renderisamos
    	return $app['twig']->render('usuarios/usuarios.twig', $params);
 });
@@ -37,21 +56,70 @@ $app->get('/usuarios', function() use ($app)
  * Post
  * 
  */
-$app->post('/usuarios', function(Request $request) use ($app) 
+$app->post('/usuario', function(Request $request) use ($app) 
 {
 	$user = $app['db']->dispense('usuarios');
 
 	$user->name = $request->get('name');
-	$user->mail = $request->get('mail');
-	$user->id_perfil = $request->get('id_perfil');
-	var_dump($user); die;
- 	try {
- 		$response['success'] = true;
- 		$response['message'] = $app['db']->store($user);
- 	} catch (Exception $e) {
+   $user->mail = $request->get('mail');
+
+   $user_id = $app['db']->store($user);
+
+	$perfiles_id = $request->get('pefil_id');
+/*
+   // do query insert maxi
+   $sql = <<<SQL
+INSERT INTO usuarios_perfiles (usuario_id, perfil_id) 
+VALUES ($user_id, $perfiles_id)
+SQL;
+   $data = $app['db']->getAll($sql);
+
+   try {
+      $response['success'] = true;
+ 	    $response['message'] = $app['db']->store($user);
+   } catch (Exception $e) {
  		$response['success'] = false;
  		$response['message'] = $e->getMessage();
  	}
-
+*/
  	return $app->json($response);
+});
+
+/**
+ * Put
+ * 
+ */
+$app->put('/usuario/{id}', function(Request $request, $id) use ($app)
+{
+   header("Access-Control-Allow-Origin: *");
+   $usuario = $app['db']->load('usuarios', $id);
+
+   $usuario->name = $request->request->get('name');
+   $usuario->mail = $request->request->get('mail');
+
+   try {
+      $response['success'] = true;
+      $response['message'] = $app['db']->store($usuario);
+   } catch (Exception $e) {
+      $response['success'] = false;
+      $response['message'] = $e->getMessage();
+   }
+   return $app->json($response);
+});
+
+/**
+ * Delete
+ * 
+ */
+$app->delete('/usuario/{id}', function($id) use ($app)
+{
+   $usuario = $app['db']->load('usuarios', $id);
+   try {
+      $response['success'] = true;
+      $response['message'] = $app['db']->trash($usuario);
+   } catch (Exception $e) {
+      $response['success'] = false;
+      $response['message'] = $e->getMessage();
+   }
+   return $app->json($response);
 });
--- www/assets/js/usuarios.js
+++ www/assets/js/usuarios.js
@@ -1,16 +1,23 @@
 var isEdit = false;
-
+var id = '';
 // on document ready
 $(document).ready(function() {
+
+	var dynatable = $('#usuarios-table');
+	
+	dynatable.bind('dynatable:afterUpdate', function(e, dynatable){
+		// hookActions();
+		console.log("Fired") ;
+	});
 
 	$.ajax({
 	  url: '/getusuarios',
 	  success: function(data) {
-	    $('#usuarios-table').dynatable({
+	    dynatable.dynatable({
 	      dataset: {
 	        records: data
 	      }
-	    });
+	    }).data('dynatable');
 	  },
 	  complete: onGetUsuariosComplete
 	});
@@ -18,17 +25,16 @@ $(document).ready(function() {
 });
 
 function onGetUsuariosComplete() {
+	hookActions();
+}
 
-	$('.edit').on('click', function(e){
-		isEdit = true;
-		var id = '';
-		if( "SPAN" === $(e.target).prop("tagName")) {
-			id = $(e.target).parent().attr('id');
-		} else {
-			id = $(e.target).attr('id');
-		}
-		console.log(id);
-		$('#usuariosForm').modal();
+function hookActions() {
+	$('.edit').on('click', function(e) {
+		// editAction(e);
+	});
+
+	$('.delete').on('click', function(e) {
+		deleteAction(e);
 	});
 
 	$('#save_user').on('click', function(e){
@@ -36,10 +42,129 @@ function onGetUsuariosComplete() {
 		$('#formSaveUser').trigger('submit');
 	});
 
+	/**
+	 * Post
+	 */
 	$('#formSaveUser').on('submit', function(e){
-		e.preventDefault();
-		console.log(this);
-		console.log(isEdit);
+		saveAction(e)
 	});
+
+	// create user
+	$('#createUser').on('click', function(e){
+		e.preventDefault();
+		$('#usuariosForm').modal();
+
+		$('#usuariosForm').on('shown.bs.modal', function(e) {
+			$.ajax({
+			  url: '/get-perfiles',
+			  success: function(data) {
+			  	$('#perfil_id').html('');
+			  	$.each(data, function() {
+			    	$('#perfil_id').append('<option value="'+this.id+'" >'+this.name+'</option>');
+			  	})
+			  },
+			  error: function(resp){
+			  	console.error(resp)
+			  }
+			});
+		});
+	});
+}
+
+function saveAction (e) {
+	e.preventDefault();
+	console.log(this);
+	console.log(isEdit);
+
+	var $form = $(this);
+	var $name = $form.find('#user_name');
+	var $mail = $form.find('#user_email');
+	var $perfil_id = $form.find('#perfil_id');
+
+	if(!isEdit){
+		$.ajax({
+			url: '/usuario',
+			method:'post',
+			data: {
+				name: $name.val(),
+				mail: $mail.val(),
+				perfil_id: $perfil_id.val()
+			},
+			success: function(response){
+				console.log(response);
+				$('button.close').trigger('click');
+			}
+		});
+	}
+}
+
+function editAction(e) {
+	isEdit = true;
+	// var id = '';
+	if( "SPAN" === $(e.target).prop("tagName")) {
+		id = $(e.target).parent().attr('id');
+	} else {
+		id = $(e.target).attr('id');
+	}
+	// disparamos el modal.
+	$('#usuariosForm').modal();
+/*
+	// nos hookeamos al evento shown.bs.modal del modal.
+	$('#usuariosForm').on('shown.bs.modal', function(e) {
+
+		var $modal = $(this);
+
+		var $name = $modal.find('#user_name');
+		var $mail = $modal.find('#user_email');
+		var $save_user = $modal.find('#save_user');
+
+		$.ajax({
+			url: '/getusuario/'+id,
+			success: function(response){
+				// console.log(response);
+				$name.val(response[0].name);
+				$mail.val(response[0].mail);
+			}
+		});
+		
+		// update
+		$save_user.on('click', function() {	
+			$.ajax({
+				url: '/usuario/'+id,
+				method:'put',
+				data: {
+					name: $name.val(),
+					mail: $mail.val()
+				},
+				success: function(response){
+					console.log(response);
+					$('button.close').trigger('click');
+					// dynatable.process();
+				}
+			});
+		});
+	});
+*/
+}
+
+function deleteAction(e) {
+	if( "SPAN" === $(e.target).prop("tagName")) {
+		id = $(e.target).parent().attr('id');
+	} else {
+		id = $(e.target).attr('id');
+	}
+
+	if (confirm('desea borrar el user ?')) {
+		/**
+		 * delete
+		 */
+		$.ajax({
+			url: '/usuario/'+id,
+			method:'delete',
+			success: function(response){
+				console.log(response);
+			}
+		});
+	}
 
 }
